1b516460f7f6650933e057df4a237fc088c399b5
AChenoll <achg7396@gmail.com>
2024-01-23 21:19:59 +0100

    ejercicio a medias

--- ejemplo/views.py
+++ ejemplo/views.py
@@ -20,4 +20,13 @@ def usuario(request):
     contexto["apellido1"]=usuario.apellido1
     contexto["apellido2"]=usuario.apellido2
 
+    contexto["lista_numeros"]=[1,2,3,4,5]
+
     return render(request, "usuario.html", (contexto))
+
+def datosUsuario(request):
+    datosUsuario=models.Test.objects.all()
+
+    contexto={"datos": datosUsuario}
+
+    return render(request, "listado_nombres.html", (contexto))
